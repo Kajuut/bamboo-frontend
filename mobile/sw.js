cde5358ee816d2cb56a4b186cead8c7879dd44f8
Kajuut <luisalexander.1057@gmail.com>
2026-06-30 15:55:31 -0500

    Commit

--- mobile/sw.js
+++ mobile/sw.js
@@ -1,2 +1,5 @@
 // sw.js - Archivo de soporte obligatorio para liberar las notificaciones en sistemas móviles
 self.addEventListener('notificationclick', (e) => e.notification.close());
+self.addEventListener('fetch', (event) => {
+    // Deja pasar las peticiones normalmente, solo sirve de pasaporte para el sistema
+});
